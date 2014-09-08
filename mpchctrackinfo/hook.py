99a562a26eb3e47f0dc7efe0738e730a960d6899
mFoxRU <mfoxru@gmail.com>
2014-09-09 01:17:58 +0400

    Added: Looking for mpc if it restarts Mix: Renamed update to get_info

--- mpchctrackinfo/hook.py
+++ mpchctrackinfo/hook.py
@@ -2,7 +2,8 @@ __author__ = 'mFoxRU'
 
 from time import sleep
 
-from win32gui import FindWindow, EnumChildWindows, GetClassName, GetWindowText
+from win32gui import FindWindow, EnumChildWindows, GetClassName, \
+    GetWindowText, IsWindow
 
 
 class Hook(object):
@@ -17,21 +18,22 @@ class Hook(object):
         self._fields = fields
         self._is_dialog = lambda x: GetClassName(x) == class_name
         self._is_infobox = lambda x: GetWindowText(x) in self._fields
-        self._dlgs = self._find_mpchc()
-        self._infobox = None
-
+        self._find_mpchc()
         self._find_infobox()
 
     def _find_mpchc(self):
         mainwindow = FindWindow(self._mpc_name, None)
+        # Waiting for mpc-hc to start
         while mainwindow == 0:
             mainwindow = FindWindow(self._mpc_name, None)
             sleep(1)
-        return self._filter_children(mainwindow, self._is_dialog)
+        self._mpc = mainwindow
+        self._dlgs = self._filter_children(self._mpc, self._is_dialog)
 
     def _find_infobox(self):
-        if self._dlgs is None:
-            self._find_mpchc()
+        if not IsWindow(self._mpc):
+            if GetClassName(self._mpc) != self._mpc_name:
+                self._find_mpchc()
         # Waiting for any track playback to find appropriate dialog box
         while 1:
             for dialog in self._dlgs:
@@ -51,7 +53,7 @@ class Hook(object):
         EnumChildWindows(window, check, None)
         return children
 
-    def update(self, reverse=True):
+    def get_info(self, reverse=True):
         if GetWindowText(self._infobox[0]) not in self._fields:
             self._find_infobox()
         retval = [GetWindowText(self._infobox[x]) for x in (1, 3)]
@@ -61,5 +63,5 @@ class Hook(object):
 if __name__ == '__main__':
     hk = Hook()
     while 1:
-        print ' - '.join(hk.update())
+        print ' - '.join(hk.get_info())
         sleep(1)
