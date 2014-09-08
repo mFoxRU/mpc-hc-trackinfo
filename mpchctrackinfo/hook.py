056855381edbf8cd8b4074668eaf7bb990eb9cd9
mFoxRU <mfoxru@gmail.com>
2014-09-09 00:55:54 +0400

    Mix: Some refactoring

--- mpchctrackinfo/hook.py
+++ mpchctrackinfo/hook.py
@@ -7,52 +7,59 @@ from win32gui import FindWindow, EnumChildWindows, GetClassName, GetWindowText
 
 class Hook(object):
 
-    fields = ['Title', 'Author']
-    is_dialog = lambda _, x: GetClassName(x) == '#32770'
-    is_infobox = lambda s, x: GetWindowText(x) in s.fields
+    def __init__(
+            self,
+            window='MediaPlayerClassicW',
+            class_name='#32770',
+            fields=('Title', 'Author')
+    ):
+        self._mpc_name = window
+        self._fields = fields
+        self._is_dialog = lambda x: GetClassName(x) == class_name
+        self._is_infobox = lambda x: GetWindowText(x) in self._fields
+        self._dlgs = self._find_mpchc()
+        self._infobox = None
 
-    def __init__(self):
-        self.dlgs = None
-        self.infobox = None
+        self._find_infobox()
 
-    def find_mpchc(self):
-        mainwindow = FindWindow('MediaPlayerClassicW', None)
+    def _find_mpchc(self):
+        mainwindow = FindWindow(self._mpc_name, None)
         while mainwindow == 0:
-            mainwindow = FindWindow('MediaPlayerClassicW', None)
+            mainwindow = FindWindow(self._mpc_name, None)
             sleep(1)
-        self.dlgs = self.filter_children(mainwindow, self.is_dialog)
+        return self._filter_children(mainwindow, self._is_dialog)
 
-    def find_infobox(self):
-        if self.dlgs is None:
-            self.find_mpchc()
+    def _find_infobox(self):
+        if self._dlgs is None:
+            self._find_mpchc()
         # Waiting for any track playback to find appropriate dialog box
         while 1:
-            for dialog in self.dlgs:
+            for dialog in self._dlgs:
                 try:
-                    if len(self.filter_children(dialog, self.is_infobox)) > 0:
-                        self.infobox = self.filter_children(dialog,
-                                                            lambda x: 1)
+                    if len(self._filter_children(dialog, self._is_infobox)) > 0:
+                        self._infobox = self._filter_children(dialog,
+                                                              lambda x: 1)
                         return
                 except Exception:
                     pass
             sleep(1)
 
-    def update(self):
-        if self.infobox is None:
-            self.find_infobox()
-        if GetWindowText(self.infobox[0]) not in self.fields:
-            self.find_infobox()
-        return GetWindowText(self.infobox[1]), GetWindowText(self.infobox[3])
-
     @staticmethod
-    def filter_children(window, condition):
+    def _filter_children(window, condition):
         children = []
         check = lambda x, _: children.append(x) if condition(x) else 1
         EnumChildWindows(window, check, None)
         return children
 
+    def update(self, reverse=True):
+        if GetWindowText(self._infobox[0]) not in self._fields:
+            self._find_infobox()
+        retval = [GetWindowText(self._infobox[x]) for x in (1, 3)]
+        return reversed(retval) if reverse else retval
+
+
 if __name__ == '__main__':
     hk = Hook()
     while 1:
-        print ' - '.join(reversed(hk.update()))
+        print ' - '.join(hk.update())
         sleep(1)
